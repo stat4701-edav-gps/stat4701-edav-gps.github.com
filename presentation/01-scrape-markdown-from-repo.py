5352b27ec03a446f4e6da66e6ffba9a1ea80343d
Daniel M Sheehan <daniel.martin.sheehan@gmailcom>
2015-04-27 18:00:02 -0400

    update

--- presentation/01-scrape-markdown-from-repo.py
+++ presentation/01-scrape-markdown-from-repo.py
@@ -1,3 +1,5 @@
 import urllib
 
-urllib.urlretrieve ("https://raw.githubusercontent.com/embruze/taxi/master/README.md", "/Users/danielmsheehan/GitHub/stat4701-edav-gps.github.com/presentation/section-emily-1.md")
+#urllib.urlretrieve ("https://raw.githubusercontent.com/embruze/taxi/master/README.md", "/Users/danielmsheehan/GitHub/stat4701-edav-gps.github.com/presentation/section-emily-1.md")
+
+urllib.urlretrieve ("https://raw.githubusercontent.com/stat4701-edav-gps/presentation/master/stats.md", "/Users/danielmsheehan/GitHub/stat4701-edav-gps.github.com/presentation/section-emily-1.md")
