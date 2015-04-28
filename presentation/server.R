89699283742221ec711f81c34a5e168f54e09f08
sg2928 <sg2928@columbia.edu>
2015-04-28 00:08:47 -0400

    updating r files

--- presentation/server.R
+++ presentation/server.R
@@ -5,14 +5,15 @@ library(ggplot2)
 #df <- read.table(file = "c:/R/project/taxi_july_boro2.csv", sep=",", header = TRUE)
 
 df <- read.table(file = "taxi_july_by_geoid.csv", sep=",", header = TRUE)
-
+ 
 
 shinyServer(function(input, output) {
 
+
   output$plot1 <- renderPlot({
 
-    df2 <- subset(df,df$BORO==input$boro & df$DIST_BLDG_HGHT<=input$Slider & df$TYPE==input$radio 
-& df$DIST_ROADBED<=input$Slider2[2] & df$DIST_ROADBED>=input$Slider2[1] )
+    df2 <- subset(df,df$BORO==input$boro & df$DIST_BLDG_HGHT<=input$Slider[2] & df$DIST_BLDG_HGHT>=input$Slider[1]
+ & df$TYPE==input$radio & df$DIST_ROADBED<=input$Slider2[2] & df$DIST_ROADBED>=input$Slider2[1] )
 #& df$GEOID>=input$geoid[1] & df$GEOID<=input$geoid[2]
     
     #df2 <- subset(df,df$BORO==input$boro & df$DIST_BLDG_HGHT==input$Slider )
@@ -30,21 +31,49 @@ shinyServer(function(input, output) {
 
   })
 
-
 output$plot2 <- renderPlot({
 
-    df3 <- subset(df,df$BORO==input$boro & df$DIST_BLDG_HGHT<=input$Slider & df$TYPE==input$radio 
-& df$DIST_ROADBED<=input$Slider2[2] & df$DIST_ROADBED>=input$Slider2[1] )
-#& df$GEOID>=input$geoid[1] & df$GEOID<=input$geoid[2]
+    df3 <- subset(df,df$BORO==input$boro & df$DIST_BLDG_HGHT<=input$Slider[2] & df$DIST_BLDG_HGHT>=input$Slider[1] 
+          & df$TYPE==input$radio & df$DIST_ROADBED<=input$Slider2[2] & df$DIST_ROADBED>=input$Slider2[1] & df$DIST_ROADBED/df$DIST_BLDG_HGHT <1 )
     
 
-    p2 <- ggplot(df3, aes( x=DIST_BLDG_HGHT,y=DIST_ROADBED)) + ylab('Value') +
-   xlab('Building Height') + geom_bar(stat = "identity")
-
+    p2 <- ggplot(df3, aes( x=DIST_BLDG_HGHT,y=DIST_ROADBED)) + geom_line() + ylab('Distance from Road Bed') +
+   xlab('Building Height')
+    
     print(p2)
 
   })
 
+output$plot3 <- renderPlot({
+
+    df4 <- subset(df,df$BORO==input$boro & df$DIST_BLDG_HGHT<=input$Slider[2] & df$DIST_BLDG_HGHT>=input$Slider[1] 
+           & df$TYPE==input$radio & df$DIST_ROADBED<=input$Slider2[2] & df$DIST_ROADBED>=input$Slider2[1] )
+    
+
+ p3 <- ggplot(df4, aes( x=DIST_BLDG_HGHT))  + geom_histogram(breaks=seq(input$Slider[1], input$Slider[2], by = 5), 
+                 col="red", 
+                 fill="green", 
+                 alpha = .2) + ylab('Count') +
+   xlab('Building Height')
+
+    print(p3)
+  })
+
+
+output$plot4 <- renderPlot({
+
+    df5 <- subset(df,df$BORO==input$boro & df$DIST_BLDG_HGHT<=input$Slider[2] & df$DIST_BLDG_HGHT>=input$Slider[1] 
+           & df$TYPE==input$radio & df$DIST_ROADBED<=input$Slider2[2] & df$DIST_ROADBED>=input$Slider2[1] )
+    
+
+ p4 <- ggplot(df5, aes( x=DIST_ROADBED))  + geom_histogram(breaks=seq(input$Slider2[1], input$Slider2[2], by = 5), 
+                 col="red", 
+                 fill="blue", 
+                 alpha = .2) + ylab('Count') +
+   xlab('Distance from Road Bed')
+
+    print(p4)
+  })
 
 })
 
